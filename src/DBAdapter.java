cf9f0c21617bfdf01ddfe8e66cf57480b3c8e536
venky91 <venksundram@gmail.com>
2013-09-09 20:37:10 -0700

    added DBAdapter.java

---
+++ src/DBAdapter.java
@@ -0,0 +1,42 @@
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;	
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;	
+import android.util.Log;
+
+public class DBAdapter extends SQLiteOpenHelper{
+
+	public static final String KEY_ROWID = "id";
+	public static final String KEY_DATE = "date";
+	public static final String KEY_SUBTOTAL = "subTotal";
+	public static final String KEY_TIP = "tip";
+	public static final String KEY_TOTAL = "total";
+	
+	private static final String DATABASE_NAME = "EntryDB";
+	private static final String DATABASE_TABLE = "entries";
+	private static final int DATABASE_VERSION = 1;
+	
+	private static final String DATABASE_CREATE = 
+			"create table if not exists " + DATABASE_TABLE + " (" + KEY_ROWID + " integer primary key " +
+					"autoincrement, " + KEY_DATE + " VARCHAR, " + KEY_SUBTOTAL + " VARCHAR, " + 
+					KEY_TIP + " VARCHAR, " + KEY_TOTAL + " VARCHAR);";
+	
+	public DBAdapter(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		
+		db.execSQL(DATABASE_CREATE);
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
